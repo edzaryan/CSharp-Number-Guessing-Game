d38545f5b7dc0b2aef3e80dca3d6068d6d098863
Edgar Yeghiazaryan <edzaryan@gmail.com>
2024-02-11 21:08:46 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+int randomNumber = new Random().Next(1, 11);
+
+bool newGame = true;
+
+while(true)
+{
+    if (newGame) {
+        Console.WriteLine("Please enter your number. [1-11]");
+        newGame = false;
+    }
+
+    uint userNumber = Convert.ToUInt32(Console.ReadLine());
+
+    if (userNumber < randomNumber)
+    {
+        Console.WriteLine("Your number is less!");
+    }
+    else if (userNumber > randomNumber)
+    {
+        Console.WriteLine("Your number is greater!");
+    }
+    else 
+    {
+        Console.WriteLine("Currect!");
+
+        Console.WriteLine("Do you want to play again? (y - yes | n - no)");
+
+        if (Convert.ToChar(Console.ReadLine()) != 'y') break;
+
+        randomNumber = new Random().Next(1, 11);
+        newGame = true;
+    }
+}
